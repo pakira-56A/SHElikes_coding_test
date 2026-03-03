d5988e569f2f5899efa974a02e71aadaaaf0af1e
pakira-56A <pakira.rrr@gmail.com>
2026-03-03 17:26:53 +0900

    add: `viewmore`ボタンのcssとアラート機能

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -38,3 +38,9 @@ ScrollReveal().reveal('.fadein', {
   },
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
+
+//クリック押下時にアラートを表示
+$('.alert-btn').on('click', function(e) {
+  alert('架空のカフェなので、アクセスできません。ごめんなさいね 🙇‍♀️');
+  e.preventDefault();
+});
